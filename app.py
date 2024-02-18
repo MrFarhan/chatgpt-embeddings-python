7e472e46528eba086247d153a8bdbb34ed5f549c
MrFarhan <muhammadfarhan012345@gmail.com>
2024-02-19 03:30:26 +0500

    Minor changes (Removed extra files)

--- app.py
+++ app.py
@@ -350,3 +350,5 @@ def ask_question():
     response = answer_question(df, question=question, debug=False)
     return jsonify({'response': response})
 
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0")
